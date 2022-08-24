e6cfbf2f8ab3b35295f76149914fd251eaff0a0c
Petr Efremov <107767949+Ventorm@users.noreply.github.com>
2022-08-24 11:18:13 +0300

    update for Heroku

--- config.js
+++ config.js
@@ -1,6 +1,7 @@
 const dotenv = require("dotenv");
+const _ = require("lodash");
 //import _ from 'lodash';
-
+/*
 const result = dotenv.config();
 
 //let envs;
@@ -12,5 +13,19 @@ const envs = result.parsed;
 //  envs = {};
 //  _.each(process.env, (value, key) => envs[key] = value);
 //}
+
+module.exports = envs;
+
+*/
+const result = dotenv.config();
+
+let envs;
+
+if (!('error' in result)) {
+  envs = result.parsed;
+} else {
+  envs = {};
+  _.each(process.env, (value, key) => envs[key] = value);
+}
 
 module.exports = envs;
