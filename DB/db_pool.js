74e937c106cccb2492978298306da77c6740369b
Petr Efremov <107767949+Ventorm@users.noreply.github.com>
2022-09-01 23:43:24 +0300

    update for Heroku

--- DB/db_pool.js
+++ DB/db_pool.js
@@ -1,9 +1,23 @@
-const { hostDB, nameDB, userDB, passwordDB } = require("../config");
+const { DATABASE_URL } = require("../config");
 const Pool = require("pg").Pool;
+
+//#region heroku DB parse
+let herokuDB = DATABASE_URL
+herokuDB = herokuDB.replaceAll(':', `_`)
+herokuDB = herokuDB.replaceAll('/', `_`)
+herokuDB = herokuDB.replaceAll('@', `_`)
+herokuDB = herokuDB.split('_')
+
+const userDB = herokuDB[3]
+const passwordDB = herokuDB[4]
+const hostDB = herokuDB[5]
+const portDB = herokuDB[6]
+const nameDB = herokuDB[7]
+//#endregion
 
 const pool = new Pool({
   host: hostDB,
-  port: 5432,
+  port: portDB,
   database: nameDB,
   user: userDB,
   password: passwordDB,
