7af390da2ce1a0d2348e49985dfe15218d58212d
Petr Efremov <107767949+Ventorm@users.noreply.github.com>
2022-10-13 16:53:32 +0300

    updated index.js

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ const { texts } = require("./Bot/texts.js");
 const { buttons } = require("./Bot/buttons.js");
 const schedule = require("node-schedule");
 const { createDateWithTargetGMT } = require(`./components/Time.js`);
+const db = require("./DB/db_pool");
 
 const {
   pollsAPI,
@@ -66,9 +67,11 @@ const devFun = async function () {
   setTimeout(async () => {
     //console.log(await pollsAPI.getAll())
     //((await gamesAPI.getAll()).data).forEach(element => console.log(element.icon));
-    //await Messages.send(admin, texts.cantToday, buttons.deleteThisMessage);
-    //await Messages.send(admin, 123, buttons.deleteThisMessage)
-    //Polls.stopAllPolls()
+    //console.log(await Messages.send(admin, texts.cantToday, buttons.deleteThisMessage))
+    //await Messages.send(admin, `<b>Погода в Канаде (Оттава) в данный момент</b>\n\n<b>За окном:</b> 18.02°, облачно с прояснениями<b>\nОщущается как:</b> 17.78°`, buttons.deleteThisMessage)
+    //await Messages.copy_send(admin, 364, admin, `Это скопированное сообщение ` + (new Date()).getSeconds())
+    //console.log(await Polls.send(admin, `1 or 2?`, [`1`, `2`]))
+    Polls.stopAllPolls()
     //!!!ниже рассылка по !Всем активным пользователям
     //(await playersAPI.getAll(true)).data.forEach((user) => {if (user.enabled) {await Messages.send(user.id, texts.forAllInfoMessage)}});
   }, 1 * 1000);
